a56905b299275175e99d90c2bc53925e325b8004
Elias Mollericona <omglasvi@gmail.com>
2026-03-16 13:07:57 -0400

    Initial commit

---
+++ src/config/envs.ts
@@ -0,0 +1,36 @@
+import 'dotenv/config'
+import * as joi from 'joi'
+
+interface EnvVars {
+  PORT: number
+  PRODUCTS_MICROSERVICES_HOST: string
+  ORDERS_MICROSERVICES_HOST: string
+  PRODUCTS_MICROSERVICES_PORT: number
+  ORDERS_MICROSERVICES_PORT: number
+}
+
+const envSchema = joi
+  .object({
+    PORT: joi.number().required(),
+    PRODUCTS_MICROSERVICES_HOST: joi.string().required(),
+    ORDERS_MICROSERVICES_HOST: joi.string().required(),
+    PRODUCTS_MICROSERVICES_PORT: joi.number().required(),
+    ORDERS_MICROSERVICES_PORT: joi.number().required(),
+  })
+  .unknown(true)
+
+const { error, value } = envSchema.validate(process.env)
+
+if (error) {
+  throw new Error(`Config validation error ${error.message}`)
+}
+
+const envVars: EnvVars = value
+
+export const envs = {
+  port: envVars.PORT,
+  productsMicroserviceHost: envVars.PRODUCTS_MICROSERVICES_HOST,
+  ordersMicroserviceHost: envVars.ORDERS_MICROSERVICES_HOST,
+  productsMicroservicePort: envVars.PRODUCTS_MICROSERVICES_PORT,
+  ordersMicroservicePort: envVars.ORDERS_MICROSERVICES_PORT,
+}
