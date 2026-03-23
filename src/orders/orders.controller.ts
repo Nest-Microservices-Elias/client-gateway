dd35a13240aad094eb7a1a5854ffd53374a4decb
Elias Mollericona <omglasvi@gmail.com>
2026-03-23 13:12:35 -0400

    Enhance RPC error handling in RpcCustomExceptionFilter and refactor findAll method in OrdersController to handle exceptions properly

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -26,8 +26,14 @@ export class OrdersController {
   }
 
   @Get()
-  findAll(@Query() orderPaginationDto: OrderPaginationDto) {
-    return this.client.send('findAllOrders', orderPaginationDto)
+  async findAll(@Query() orderPaginationDto: OrderPaginationDto) {
+    try {
+      return await firstValueFrom(
+        this.client.send('findAllOrders', orderPaginationDto),
+      )
+    } catch (error) {
+      throw new RpcException(error)
+    }
   }
 
   @Get('id/:id')
